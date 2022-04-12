0c966b4b5d1902d985afe8fe9dd78f84d00902a4
M0SKITOCY <igorbir1408@gmail.com>
2022-04-12 15:01:01 +1000

    Shooting (for start)

---
+++ insect_rage/Assets/Scripts/HeroShoot.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class HeroShoot : MonoBehaviour
+{
+    public GameObject projectile;
+    public float fireDelta = 0.5F;
+    
+    private float nextFire = 0.5F;
+    private GameObject newProjectile;
+    private float myTime = 0.0F;
+
+    void Update()
+    {
+        myTime = myTime + Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && myTime > nextFire)
+        {
+            nextFire = myTime + fireDelta;
+            newProjectile = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+            nextFire = nextFire - myTime;
+            myTime = 0.0F;
+        }
+    }
+}
